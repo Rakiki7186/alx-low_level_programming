64361e3ed4cfc424450188c00a0f93fb804b08c7
rakiki7186 <rakebtesfayee@gmail.com>
2023-06-20 21:11:45 -0700

    1-main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Determine if a random number is positive, negative or zero.
+ *
+ * Returns: 0 on success
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is %s\n", n, "positive");
+}
+else if (n < 0)
+{
+printf("%d is %s\n", n, "negative");
+}
+else
+{
+printf("%d is %s\n", n, "zero");
+}
+return (0);
+}
