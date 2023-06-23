1631a47f8378f94cf0cadc0ed57da03042272fa3
rakiki7186 <rakebtesfayee@gmail.com>
2023-06-22 17:41:47 -0700

    4-print editrd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * print_most_numbers - entry point
+ *
  * Description: Prints the numbers with _putchar
+ *
  * Return: void
  */
 void print_most_numbers(void)
@@ -10,6 +12,7 @@ int number;
 for (number = '0'; number <= '9'; number++)
 {
 if (number == '2' || number == '4')
+;
 else
 _putchar(number);
 }
